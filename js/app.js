c5fcc2f432207ba889388b16e85438f9141a7281
JGALLEGO1234 <joshhbusinesss@gmail.com>
2025-03-26 19:52:31 -0600

    funcion para detectar agregar carrito

--- js/app.js
+++ js/app.js
@@ -1,6 +1,19 @@
-//Vairables de capturas del DOM
-
+//Variables de captura del DOM o selectores
 const vaciarCarrito = document.querySelector("#vaciar-carrito");
-const listaArticulos =document.querySelector("#lista-articulos");
+const listaArticulos = document.querySelector("#lista-articulos");
 const carrito = document.querySelector("#carrito");
-const divCarrito = document.querySelector("#lista-carrito tbody");   
+const divCarrito = document.querySelector("#lista-carrito tbody");
+
+//registrar eventos
+registrarEventListeners();
+function registrarEventListeners() {
+    listaArticulos.addEventListener("click", agregarArticulo);
+}
+
+// funciones
+function agregarArticulo(e) {
+    console.log(e.target.classList);
+}
+
+
+
