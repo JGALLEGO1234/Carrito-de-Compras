43130ec8ba9e37569ee5a63ca5e7327a2aea19dd
JGALLEGO1234 <joshhbusinesss@gmail.com>
2025-03-26 19:15:55 -0600

    Adding this just as backup lmao

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
 //Vairables de capturas del DOM
 
 const vaciarCarrito = document.querySelector("#vaciar-carrito");
+const listaArticulos =document.querySelector("#lista-articulos");
+
+function clickArticulos(){
+    console.log("OSTRASSSSSSSSSS diste click en cualquier lugar de la lista articulos kepro");
+}
+
+listaArticulos.addEventListener("click", clickArticulos);
