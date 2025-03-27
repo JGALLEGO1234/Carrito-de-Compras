06f880692e7430909fa7355bd38e8cd11567a25c
JGALLEGO1234 <joshhbusinesss@gmail.com>
2025-03-26 19:22:52 -0600

    All we need to start adding to the cart

--- js/app.js
+++ js/app.js
@@ -2,9 +2,5 @@
 
 const vaciarCarrito = document.querySelector("#vaciar-carrito");
 const listaArticulos =document.querySelector("#lista-articulos");
-
-function clickArticulos(){
-    console.log("OSTRASSSSSSSSSS diste click en cualquier lugar de la lista articulos kepro");
-}
-
-listaArticulos.addEventListener("click", clickArticulos);
+const carrito = document.querySelector("#carrito");
+const divCarrito = document.querySelector("#lista-carrito tbody");   
