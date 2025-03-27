845beaf912a3ed6e6a164ab395637ec0907aa98d
JGALLEGO1234 <joshhbusinesss@gmail.com>
2025-03-27 14:06:58 -0600

    Agregando cambios a la terminal hasta ver donde realmente se hace para separar la funcion agregar carrito

--- js/app.js
+++ js/app.js
@@ -1,13 +1,24 @@
-//Variables  de captura del DOM o selectores
-
+//Variables  de captura del DOM o selectores para saber que aprte del DOM se ataca //Estos cuatro son todos los selectores necesarios para el funcionamiento del carrito
 const vaciarCarrito = document.querySelector("#vaciar-carrito");
-const listaArticulos = document.querySelector("#lista-articulos");             //Estos cuatro son todos los selectores necesarios para el funcionamiento del carrito
+const listaArticulos = document.querySelector("#lista-articulos");       //Esto es absolutamente toda la lista de articulos
 const carrito = document.querySelector("#carrito");
 const divCarrito = document.querySelector("#lista-carrito");
 
+//registrar eventos
+registerEventListeners();
+function registerEventListeners(){                 //Esto va a mostrar cualquier click que haga sobre la tarjeta.
+    listaArticulos.addEventListener('click', agregarArticulo);         //Aqui se registra el evento de click en la lista de articulos
+}                                  //Aqui va la funcion o las intrucciones
 
+//---------------------------------------
+//Funciones
+/* function agregarArticulo(){
+    console.log('Hiciste click en articulos')     //Esto seria sin eventos
+} */
 
-
+function agregarArticulo(e){
+    console.log(e.target);   //Recordemos que e es EVENTO   //Este detecta especificamente si lke doy click a un titulo o imagen, y lo muestra en consola
+}
 
 
 
@@ -24,6 +35,6 @@ const divCarrito = document.querySelector("#lista-carrito");
 
 
 /* function clickArticulos(){
-    console.log("diste click en cualquier lugar de la lista de articulos");            //Esto sirve para ver en consola que se ha clickado el boton en cualqueir lugar de las tarjetas
+    console.log("diste click en cualquier lugar de la lista de articulos");     //Esto sirve para ver en consola que se ha clickado el boton en cualqueir lugar de las tarjetas
 }
 listaArticulos.addEventListener("click", clickArticulos); */
